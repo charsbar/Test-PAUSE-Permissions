d4dccdb47024c730c8d04efc8c850d2495e3ab2c
Kenichi Ishigaki <ishigaki@cpan.org>
2014-04-11 14:48:13 +0900

    releng 0.05

--- lib/Test/PAUSE/Permissions.pm
+++ lib/Test/PAUSE/Permissions.pm
@@ -7,7 +7,7 @@ use Test::More;
 use PAUSE::Permissions;
 use Parse::LocalDistribution;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 our @EXPORT = (@Test::More::EXPORT, qw/all_permissions_ok/);
 
